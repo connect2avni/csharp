7e4d0a5a2d5fe15cb5f725c1d90bce56f0f4d0f6
Mohapatra <smruti-snigdha.mohapatra@capgemini.com>
2024-01-29 02:09:14 +0530

    updated

--- program.cs
+++ program.cs
@@ -5,6 +5,7 @@ namespace Container
 {
     class Program
     {
+
         // class variable | class member
         private string name; // field
 
@@ -286,6 +287,96 @@ namespace Container
             Object Obj = num;    // Boxing
             int i = (int)Obj;    // Unboxing
 
+
+            // arraylist
+            int intValue = 100;
+            double doubleValue = 20.5;
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add("John");
+            arrayList.Add(intValue);
+            arrayList.Add(doubleValue);
+            for (int index = 0; index < arrayList.Count; index++)
+                Console.WriteLine(arrayList[index]);
+
+
+            // stack
+            Stack stackObject = new Stack();
+            stackObject.Push("Joydip");
+            stackObject.Push("Steve");
+            stackObject.Push("Jini");
+            while (stackObject.Count > 0) Console.WriteLine(stackObject.Pop());
+            Console.ReadLine();
+
+            // queue
+            Queue queueObject = new Queue();
+            queueObject.Enqueue("Joydip");
+            queueObject.Enqueue("Steve");
+            queueObject.Enqueue("Jini");
+            while (queueObject.Count > 0) Console.WriteLine(queueObject.Dequeue());
+            Console.ReadLine();
+
+            // Hashtable
+            Hashtable hashTable = new Hashtable();
+            hashTable.Add(1, "Joydip");
+            hashTable.Add(2, "Manashi");
+            hashTable.Add(3, "Jini");
+            hashTable.Add(4, "Piku");
+            Console.WriteLine("The keys and values are:");
+            foreach (int k in hashTable.Keys)
+            {
+                Console.WriteLine(k);
+                Console.WriteLine(hashTable[k].ToString());
+            }
+
+            // string builder | stringbuilder
+            StringBuilder s = new StringBuilder("GeeksforGeeks");
+            StringBuilder s1 = new StringBuilder("GeeksForGeeks", 20);
+            s.Append("GFG");
+            s.AppendLine("GEEKS");
+            s.AppendFormat("{0:C} ", 50);
+            s.Insert(6, "GEEKS");
+            // remove starts from index 5 
+            // and remove happes 3 index  
+            // after index 5 
+            s.Remove(5, 3);
+            // Replace "GFG" with "Geeks For" 
+            s.Replace("GFG", "Geeks For");
+
+            // check if string is equal
+            string s1 = "ProGeek 2.0";
+            string s2 = "ProGeek 2.0";
+            // Equals() method return true
+            // as both string objects are equal
+            Console.WriteLine(s1.Equals(s2));
+
+            // check if string is equal
+            string s1 = "ProGeek 2.0";
+            string s2 = "ProGeek 2.0";
+            // CompareTo() method return 0
+            // as both string objects are equal
+            Console.WriteLine(s1.CompareTo(s2));
+
+            // anonymous types | anonymous object
+            var student = new { Id = 1, FirstName = "James", LastName = "Bond" };
+
+            // Nested Anonymous Type
+            var student = new
+            {
+                Id = 1,
+                FirstName = "James",
+                LastName = "Bond",
+                Address = new { Id = 1, City = "London", Country = "UK" }
+            };
+
+
+            // Array of Anonymous Types
+            var students = new[] {
+                        new { Id = 1, FirstName = "James", LastName = "Bond" },
+                        new { Id = 2, FirstName = "Steve", LastName = "Jobs" },
+                        new { Id = 3, FirstName = "Bill", LastName = "Gates" }
+                };
+
+
         }
 
         // method overloading | same method name but different signature
@@ -387,7 +478,7 @@ namespace Container
     }
 
 
-    // Polymorphism: override -- dont forget to use 'virtual' and 'override' keywords
+    // Polymorphism: override -- dont forget to use 'virtual' and 'override' keywords | override keyword | method overriding
     class Animal  // Base class (parent) 
     {
         public virtual void animalSound()
@@ -453,4 +544,323 @@ namespace Container
         }
     }
 
+    // The yield Statement
+    public class DaysOfTheWeek : System.Collections.IEnumerable
+    {
+        string[] m_Days = { "Sun", "Mon", "Tue", "Wed", "Thr", "Fri", "Sat" };
+        public System.Collections.IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < m_Days.Length; i++)
+            {
+                yield return m_Days[i];
+            }
+        }
+    }
+
+
+    // Iterators: Example
+
+    class TestDaysOfTheWeek
+    {
+        static void Main()
+        {
+            DaysOfTheWeek week = new DaysOfTheWeek();
+            foreach (string day in week)
+            {
+                System.Console.Write(day + " ");
+            }
+        }
+    }
+
+
+    // Sealed class -- cannot be inherited
+    sealed class SealedClass
+    {
+
+        // Calling Function
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    // Define Generic Class
+    class DataStore<T>
+    {
+        public T Data { get; set; }
+    }
+
+    // Generic Class with Multiple Type Parameters
+    class KeyValuePair<TKey, TValue>
+    {
+        public TKey Key { get; set; }
+        public TValue Value { get; set; }
+    }
+
+
+
+
+
+    // extension methods
+    class Geek
+    {
+
+        // Method 1 
+        public void M1()
+        {
+            Console.WriteLine("Method Name: M1");
+        }
+
+        // Method 2 
+        public void M2()
+        {
+            Console.WriteLine("Method Name: M2");
+        }
+
+        // Method 3 
+        public void M3()
+        {
+            Console.WriteLine("Method Name: M3");
+        }
+
+    }
+
+    static class NewMethodClass
+    {
+
+        // Method 4 
+        public static void M4(this Geek g)
+        {
+            Console.WriteLine("Method Name: M4");
+        }
+
+        // Method 5 
+        public static void M5(this Geek g, string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+
+    class MyClient
+    {
+        public static void Main()
+        {
+            try
+            {
+                throw new MyException(“Some error has happened");
+            }
+            catch (MyException e)
+            {
+                Console.WriteLine("Exception caught here" + e.ToString());
+            }
+            Console.WriteLine("LAST STATEMENT");
+        }
+    }
+
+}
+
+
+namespace GeeksForGeeks
+{
+
+    // declare class "Geeks"
+    class Geeks
+    {
+
+        // Declaring the delegates
+        // Here return type and parameter type should 
+        // be same as the return type and parameter type
+        // of the two methods
+        // "addnum" and "subnum" are two delegate names
+        public delegate void addnum(int a, int b);
+        public delegate void subnum(int a, int b);
+
+        // method "sum"
+        public void sum(int a, int b)
+        {
+            Console.WriteLine("(100 + 40) = {0}", a + b);
+        }
+
+        // method "subtract"
+        public void subtract(int a, int b)
+        {
+            Console.WriteLine("(100 - 60) = {0}", a - b);
+        }
+
+        // Main Method
+        public static void Main(String[] args)
+        {
+
+            // creating object "obj" of class "Geeks"
+            Geeks obj = new Geeks();
+
+            // creating object of delegate, name as "del_obj1" 
+            // for method "sum" and "del_obj2" for method "subtract" &
+            // pass the parameter as the two methods by class object "obj"
+            // instantiating the delegates
+            addnum del_obj1 = new addnum(obj.sum);
+            subnum del_obj2 = new subnum(obj.subtract);
+
+            // pass the values to the methods by delegate object
+            del_obj1(100, 40);
+            del_obj2(100, 60);
+
+            // These can be written as using
+            // "Invoke" method
+            // del_obj1.Invoke(100, 40);
+            // del_obj2.Invoke(100, 60);
+        }
+    }
+
+    class GFG
+    {
+
+        public delegate void petanim(string pet);
+
+        // Main method 
+        static public void Main()
+        {
+
+            // An lambda method with one parameter 
+            petanim p = delegate (string mypet)
+            {
+                Console.WriteLine("My favorite pet is: {0}", mypet);
+            };
+            p("Dog");
+
+            // An lambda method with one parameter 
+            petanim p = delegate (string mypet) =>
+            {
+                Console.WriteLine("My favorite pet is: {0}", mypet);
+            };
+            p("Dog");
+
+
+            // file reader, streamreader, stream reader
+            try
+            {
+                // Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+                using (StreamReader sr = new StreamReader("c:/jamaica.txt"))
+                {
+                    string line;
+
+                    // Read and display lines from the file until 
+                    // the end of the file is reached. 
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                // Let the user know what went wrong.
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+
+    // streamwriter, file writer, stream writer
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[] { "Zara Ali", "Nuha Ali" };
+
+            using (StreamWriter sw = new StreamWriter("names.txt"))
+            {
+
+                foreach (string s in names)
+                {
+                    sw.WriteLine(s);
+                }
+            }
+
+            // Read and show each line from the file.
+            string line = "";
+            using (StreamReader sr = new StreamReader("names.txt"))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+
+
+    // Binary reader, binaryreader, binarywriter, binary writer
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryWriter bw;
+            BinaryReader br;
+
+            int i = 25;
+            double d = 3.14157;
+            bool b = true;
+            string s = "I am happy";
+
+            //create the file
+            try
+            {
+                bw = new BinaryWriter(new FileStream("mydata", FileMode.Create));
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message + "\n Cannot create file.");
+                return;
+            }
+
+            //writing into the file
+            try
+            {
+                bw.Write(i);
+                bw.Write(d);
+                bw.Write(b);
+                bw.Write(s);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message + "\n Cannot write to file.");
+                return;
+            }
+            bw.Close();
+
+            //reading from the file
+            try
+            {
+                br = new BinaryReader(new FileStream("mydata", FileMode.Open));
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message + "\n Cannot open file.");
+                return;
+            }
+
+            try
+            {
+                i = br.ReadInt32();
+                Console.WriteLine("Integer data: {0}", i);
+                d = br.ReadDouble();
+                Console.WriteLine("Double data: {0}", d);
+                b = br.ReadBoolean();
+                Console.WriteLine("Boolean data: {0}", b);
+                s = br.ReadString();
+                Console.WriteLine("String data: {0}", s);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message + "\n Cannot read from file.");
+                return;
+            }
+            br.Close();
+            Console.ReadKey();
+        }
+    }
 }
